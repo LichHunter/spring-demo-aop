8268863f8f856bc72687c21aee67bff3fd3e311d
AlexD2019 <alexander0derevianko@gmail.com>
2021-03-31 15:13:24 +0300

    Creating Pointcut declaration and combining them

--- src/main/java/com/luv2code/springdemo/springdemoaop/SpringDemoAopApplication.java
+++ src/main/java/com/luv2code/springdemo/springdemoaop/SpringDemoAopApplication.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo.springdemoaop;
 
 import com.luv2code.springdemo.springdemoaop.dao.AccountDAO;
+import com.luv2code.springdemo.springdemoaop.dao.MembershipDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,8 +12,9 @@ public class SpringDemoAopApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringDemoAopApplication.class, args);
 		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 
 		accountDAO.addAccount();
-		accountDAO.addAccount();
+		membershipDAO.addAccount();
 	}
 }
--- src/main/java/com/luv2code/springdemo/springdemoaop/aspect/LoggingAspect.java
+++ src/main/java/com/luv2code/springdemo/springdemoaop/aspect/LoggingAspect.java
@@ -2,12 +2,25 @@ package com.luv2code.springdemo.springdemoaop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
-	@Before("execution(public void addAccount())")
+	@Pointcut("execution(public void com.luv2code.springdemo.springdemoaop.dao.*.*())")
+	private void forDaoPackage() {
+	}
+
+	@Pointcut("execution(public void com.luv2code.springdemo.springdemoaop.*.*.*(..))")
+	private void forAllClasses() {
+	}
+
+
+	//execution(modifiers-pattern? return-type-pattern declaring-type-pattern?
+	// method-name-pattern(param-pattern) throws-pattern?)
+	// method-name-pattern(*(one any argument)/..(any number of parameters))
+	@Before("forDaoPackage() || !forAllClasses()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\nExecuting before method addAccount");
 	}
